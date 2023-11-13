8d369e8eb547fec7b938af8a9b5339abe0c77e78
HenryNg101 <hungnguyenquoc8a2@gmail.com>
2023-11-13 18:21:40 +1030

    Add script for easier testing

--- maelstrom-echo/main.go
+++ maelstrom-echo/main.go
@@ -27,23 +27,6 @@ func main() {
 		return n.Reply(msg, body)
 	})
 
-	n.Handle("generate", func(msg maelstrom.Message) error {
-		// Unmarshal the message body as an loosely-typed map.
-		var body map[string]any
-
-		// Unpack the message "body" part from received JSON, if the unpacking has problems, it will return error.
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		// Update the message type to return back.
-		body["type"] = "generate_ok"
-		body["id"] = 1 //Testing value
-
-		// Echo the original message back with the updated message type.
-		return n.Reply(msg, body)
-	})
-
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
